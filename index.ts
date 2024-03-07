513f184f25ce4085ec4b49c21c0e2756bc209752
Kaitlyn Ip <kaitlyn.ip>
2024-03-07 14:56:10 +0000

    added unformatted code

--- index.ts
+++ index.ts
@@ -1,48 +1,59 @@
+ertainly! If you want to assign the string representation of `randomNumber` to the variable `computer`, you can modify the code like this:
+
+	```javascript
 /* This program is the Rock-Paper-Scissors game
- *
- * By: Kaitlyn Ip
- * Version: 1.0
- * Since: 2024-03-07
- */
+*  *
+	*   * By: Kaitlyn Ip
+*    * Version: 1.0
+*     * Since: 2024-03-07
+*      */
 
-import promptSync from "prompt-sync";
-import random from "random";
-
- // The program will halt with a response at improper user inputs
- const prompt = promptSync();
-
- const randomNumber = random.int(1, 3);
- // input
- const player = prompt("Decide on a move (rock, paper, or scissors): ");
- let computer = String(randomNumber);
- // process & output
- if (randomNumber === 1) {
-   computer = "rock";
- } else if (randomNumber === 2) {
-   computer = "paper";
- } else if (randomNumber === 3) {
-   computer = "scissors";
- }
-
- if (player === computer) {
-   console.log(`\nComputer chose ${computer}.`);
-   console.log("\nTie.");
- } else if (
-   (player === "rock" && computer === "paper") ||
-   (player === "paper" && computer === "scissors") ||
-   (player === "scissors" && computer === "rock")
- ) {
-   console.log(`\nComputer chose ${computer}.`);
-   console.log("\nYou lost.");
- } else if (
-   (player === "paper" && computer === "rock") ||
-   (player === "scissors" && computer === "paper") ||
-   (player === "rock" && computer === "scissors")
- ) {
-   console.log(`\nComputer chose ${computer}.`);
-   console.log("\nYou win!");
- } else {
-   console.log("\nInvalid input.");
- }
- 
- console.log("\nDone.");
+// Importing necessary functions
+// import { createPrompt } from 'bun-promptx'
+//
+// // Creating prompt function
+// const prompt = createPrompt()
+//
+// // Generating random number between 1 and 3
+// const randomNumber = Math.floor(Math.random() * 3) + 1
+//
+// // Input
+// const player = prompt("Decide on a move (rock, paper, or scissors): ")
+//
+// let computer = String(randomNumber)
+//
+// // Assigning computer's move based on the generated random number
+// if (randomNumber === 1) {
+//   computer = "rock"
+//   } else if (randomNumber === 2) {
+//     computer = "paper"
+//     } else if (randomNumber === 3) {
+//       computer = "scissors"
+//       }
+//
+//       // Process & Output
+//       if (player === computer) {
+//         console.log(`\nComputer chose ${computer}.`)
+//           console.log("\nTie.")
+//           } else if (
+//             (player === "rock" && computer === "paper") ||
+//               (player === "paper" && computer === "scissors") ||
+//                 (player === "scissors" && computer === "rock")
+//                 ) {
+//                   console.log(`\nComputer chose ${computer}.`)
+//                     console.log("\nYou lost.")
+//                     } else if (
+//                       (player === "paper" && computer === "rock") ||
+//                         (player === "scissors" && computer === "paper") ||
+//                           (player === "rock" && computer === "scissors")
+//                           ) {
+//                             console.log(`\nComputer chose ${computer}.`)
+//                               console.log("\nYou win!")
+//                               } else {
+//                                 console.log("\nInvalid input.")
+//                                 }
+//
+//                                 console.log("\nDone.")
+//                                 ```
+//
+//                                 With this change, the `computer` variable will hold the string representation of `randomNumber`, and it will be further reassigned based on the conditionals below.
