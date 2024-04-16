301ab68180ad16f6041d7887980f4af292882fae
Kaitlyn Ip <kaitlyn.ip>
2024-04-16 16:03:56 +0000

    added code java

--- RockPaperScissors.java
+++ RockPaperScissors.java
@@ -1,85 +1,84 @@
-/*
- * This program is the Rock-Paper-Scissors game
- *
- * By: Kaitlyn Ip 
- * Version: 1.0
- * Since: 2024-03-07
- */
+/* this is a program that plays Rock, Paper, Scissors.
+*
+* @author  Kaitlyn Ip
+* @version 1.0
+* @since   2024-01-01
+*/
 
+import java.util.Random;
 import java.util.Scanner;
 
 /**
- * The starting function.
- *
- */
-
+* This is the standard RockPaperScissors program.
+*/
 final class RockPaperScissors {
 
     /**
-     * Rock constant, 1.
-     */
-    public static final String ROCK = "rock";
-
-    /**
-     * Paper constant, 1.
-     */
-    public static final String PAPER = "paper";
-
-    /**
-     * Scissors constant, 1.
-     */
-    public static final String SCISSORS = "scissors";
-
-    /**
-     * Prevent instantiation.
-     * Throw an exception IllegalStateException.
-     * if this is ever called
-     *
-     * @throws IllegalStateException if this is ever called
-     *
-     */
-
+    * Prevent instantiation.
+    * Throw an exception IllegalStateException.
+    * if this is ever called
+    *
+    * @throws IllegalStateException if this is ever called
+    *
+    */
     private RockPaperScissors() {
         throw new IllegalStateException("Cannot be instantiated");
     }
 
     /**
-     * The starting main() function.
-     *
-     * @param args No args will be used
-     */
+    * This function simulates a game of "rock-paper-scissors".
+    *
+    * @param userChoice  int for player refering to rock, paper, or scissors
+    * @param computerChoice  int for computer: rock, paper, or scissors
+    *
+    * @return The outcome of the game.
+    */
+    public static String rockPaperScissors(int userChoice, int computerChoice) {
+        String output = "";
+        final int numberOfChoices = 3;
+        if (userChoice == computerChoice) {
+            output = "tie";
+        } else if ((userChoice + 1) % numberOfChoices == computerChoice) {
+            output = "lose";
+        } else {
+            output = "win";
+        }
+        return output;
+    }
+
+    /**
+    * The starting main() function.
+    *
+    * @param args No args will be used
+    */
     public static void main(String[] args) {
-        // Generating random number between 0 and 2
-        final Random randomNum = new Random();
-        final int randomNumber = randomNum.nextInt(3);
+        // variables
+        final String[] choices = {"rock", "paper", "scissors"};
+        String output = "";
+
+        final Random random = new Random();
+        final int computerChoice = random.nextInt(3);
+
         // Input
         final Scanner scanner = new Scanner(System.in);
-        System.out.print("Decide on a move (rock paper, or scissors): ");
-        final String player = scanner.nextLine().toLowerCase();
-        // Assigning computer's move based on the generated random number
-        final String[] moves = {ROCK, PAPER, SCISSORS};
-        final String computer = moves[randomNumber];
-        // Process & Output
-        if (player.equals(computer)) {
-            System.out.println("\nComputer chose " + computer);
-            System.out.println("\nTie.");
-        } else if (
-            ROCK.equals(player) && PAPER.equals(computer)
-                || PAPER.equals(player) && SCISSORS.equals(computer)
-                || SCISSORS.equals(player) && ROCK.equals(computer)
-        ) {
-            System.out.println("\nThe computer chose " + computer);
-            System.out.println("\nYou lost.");
-        } else if (
-            PAPER.equals(player) && ROCK.equals(computer)
-                || SCISSORS.equals(player) && PAPER.equals(computer)
-                || ROCK.equals(player) && SCISSORS.equals(computer)
-        ) {
-            System.out.println("\nThe Computer chose " + computer);
-            System.out.println("\nYou win!");
+        System.out.print("Decide on a move(rock, paper, or scissors): ");
+        final String userInputString = scanner.nextLine().trim();
+
+        // Error check
+        if (userInputString.equals(choices[0])) {
+            output = rockPaperScissors(0, computerChoice);
+        } else if (userInputString.equals(choices[1])) {
+            output = rockPaperScissors(1, computerChoice);
+        } else if (userInputString.equals(choices[2])) {
+            output = rockPaperScissors(2, computerChoice);
         } else {
-            System.out.println("\nInvalid input.");
+            output = "Invalid input.";
         }
+        System.out.println(
+            "Computer chose "
+            + choices[computerChoice] + ". You " + output
+        );
+
         System.out.println("\nDone.");
     }
 }
